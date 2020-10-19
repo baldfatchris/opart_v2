c483fb55436629a41d45ab3695c4e8f7715a29e0
Chris Foulkes <chris@foulk.es>
2020-10-19 10:59:04 +0100

    Update Fibonacci

--- lib/model.dart
+++ lib/model.dart
@@ -15,14 +15,34 @@ class SettingsModelDouble {
 
   SettingsModelDouble({this.label, this.tooltip, this.icon, this.min, this.max, this.defaultValue, this.value});
 
-
-
   void randomise(Random rnd){
     if (this.locked == false) {
       this.value =  rnd.nextDouble() * (this.max - this.min) + this.min;
     }
   }
 }
+
+
+class SettingsModelInt {
+  int value;
+  bool locked = false;
+  final String label;
+  final String tooltip;
+  final Icon icon;
+  final int min;
+  final int max;
+  final int defaultValue;
+
+  SettingsModelInt({this.label, this.tooltip, this.icon, this.min, this.max, this.defaultValue, this.value});
+
+  void randomise(Random rnd){
+    if (this.locked == false) {
+      this.value =  rnd.nextInt(this.max - this.min) + this.min;
+    }
+  }
+}
+
+
 
 
 
