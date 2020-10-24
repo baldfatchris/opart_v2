3245fd5530d0d05ba07194009a30a3379e20d5bd
Emily Smithson <emily_foulkes@hotmail.com>
2020-10-24 10:02:58 +0100

    pro features in toolbox

--- lib/toolbox.dart
+++ lib/toolbox.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'settings_dialog.dart';
+import 'model.dart';
 
 void ToolBox(BuildContext context, List settingsList, Function cache) {
   print(MediaQuery.of(context).size.width);
@@ -24,7 +25,42 @@ void ToolBox(BuildContext context, List settingsList, Function cache) {
                       childAspectRatio: 1.3),
                   itemCount: settingsList.length,
                   itemBuilder: (BuildContext context, int index) {
-                    return GestureDetector(
+                    return (settingsList[index].proFeature|| proVersion)?
+                    Stack(
+                      children: [
+                        Center(
+                          child: GestureDetector(
+                            onTap: () {
+                              Navigator.pop(context);
+                              settingsDialog(context, index, settingsList, cache);
+                            },
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                settingsList[index].icon,
+                                Padding(
+                                  padding: const EdgeInsets.all(4.0),
+                                  child: Container(
+                                      height: 40,
+                                      child: Text(
+                                        settingsList[index].label,
+                                        textAlign: TextAlign.center,
+                                      )),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),Container(color: Colors.white.withOpacity(0.5)),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 8.0),
+                          child: Align(alignment: Alignment.topRight,child:(Icon(Icons.lock, color: Colors.cyan[200].withOpacity(0.8)))),
+                        )
+                      ],
+                    )
+
+                    :GestureDetector(
                       onTap: () {
                         Navigator.pop(context);
                         settingsDialog(context, index, settingsList, cache);
