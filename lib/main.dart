85b921de41887ee6ad5116facdcfd9133af1d912
Chris Foulkes <chris@foulk.es>
2020-12-01 03:45:38 +0000

    Added Rhombus

--- lib/main.dart
+++ lib/main.dart
@@ -294,6 +294,7 @@ class _MyHomePageState extends State<MyHomePage> {
       OpArtTypes('Shapes', OpArtType.Shapes, 'lib/assets/shapes_500.png'),
       OpArtTypes('Maze', OpArtType.Maze, 'lib/assets/maze_500.png'),
       OpArtTypes('Quads', OpArtType.Quads, 'lib/assets/quads_500.png'),
+      OpArtTypes('Rhombus', OpArtType.Rhombus, 'lib/assets/rhombus_500.png'),
       OpArtTypes('Riley', OpArtType.Riley, 'lib/assets/riley_500.png'),
       OpArtTypes('Squares', OpArtType.Squares, 'lib/assets/squares_500.png'),
       OpArtTypes('Triangles', OpArtType.Triangles, 'lib/assets/triangles_500.png'),
