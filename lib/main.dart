410166d433864f2192a8ef44b2bc81a15225dbe6
Chris Foulkes <chris@foulk.es>
2020-12-05 06:30:28 +0000

    Added Plasma

--- lib/main.dart
+++ lib/main.dart
@@ -308,7 +308,7 @@ class _MyHomePageState extends State<MyHomePage> {
        OpArtTypes('Neighbour', OpArtType.Neighbour, 'lib/assets/neighbour_500.png'),
       OpArtTypes('Maze', OpArtType.Maze, 'lib/assets/maze_500.png'),
       OpArtTypes('Quads', OpArtType.Quads, 'lib/assets/quads_500.png'),
-      OpArtTypes('Plasma', OpArtType.Plasma, 'lib/assets/quads_500.png'),
+      OpArtTypes('Plasma', OpArtType.Plasma, 'lib/assets/plasma_500.png'),
       OpArtTypes('Rhombus', OpArtType.Rhombus, 'lib/assets/rhombus_500.png'),
       OpArtTypes('Riley', OpArtType.Riley, 'lib/assets/riley_500.png'),
       OpArtTypes('Triangles', OpArtType.Triangles, 'lib/assets/triangles_500.png'),
