b683eeca5181b5445a121f9377fda608fb6bb328
Chris Foulkes <chris@foulk.es>
2020-12-05 06:23:52 +0000

    Added Plasma

---
+++ lib/opart/opart_life.dart
@@ -0,0 +1,271 @@
+import 'package:flutter/material.dart';
+import 'package:opart_v2/opart_icons.dart';
+import '../model_opart.dart';
+import '../model_palette.dart';
+import '../model_settings.dart';
+import 'dart:math';
+import 'dart:core';
+import '../main.dart';
+
+// List<String> list = List();
+
+List squaresI;
+
+SettingsModel reDraw = SettingsModel(
+  name: 'reDraw',
+  settingType: SettingType.button,
+  label: 'Redraw',
+  tooltip: 'Re-draw the picture with a different random seed',
+  defaultValue: false,
+  icon: Icon(Icons.refresh),
+  settingCategory: SettingCategory.tool,
+  proFeature: false,
+  onChange: (){seed = DateTime.now().millisecond;},
+  silent: true,
+);
+
+SettingsModel zoomOpArt = SettingsModel(
+  name: 'zoomOpArt',
+  settingType: SettingType.double,
+  label: 'Zoom',
+  tooltip: 'Zoom in and out',
+  min: 1.0,
+  max: 50.0,
+  zoom: 100,
+  defaultValue: 5.0,
+  icon: Icon(Icons.zoom_in),
+  settingCategory: SettingCategory.tool,
+  proFeature: false,
+);
+
+
+
+SettingsModel randomColors = SettingsModel(
+  name: 'randomColors',
+  settingType: SettingType.bool,
+  label: 'Random Colors',
+  tooltip: 'randomize the colours',
+  defaultValue: true,
+  icon: Icon(Icons.gamepad),
+  settingCategory: SettingCategory.tool,
+  proFeature: false,
+  silent: true,
+);
+
+SettingsModel paletteType = SettingsModel(
+  name: 'paletteType',
+  settingType: SettingType.list,
+  label: "Palette Type",
+  tooltip: "The nature of the palette",
+  defaultValue: "random",
+  icon: Icon(Icons.colorize),
+  options: [
+    'random',
+    'blended random',
+    'linear random',
+    'linear complementary'
+  ],
+  settingCategory: SettingCategory.palette,
+  onChange: (){generatePalette();},
+  proFeature: false,
+);
+SettingsModel paletteList = SettingsModel(
+  name: 'paletteList',
+  settingType: SettingType.list,
+  label: "Palette",
+  tooltip: "Choose from a list of palettes",
+  defaultValue: "Default",
+  icon: Icon(Icons.palette),
+  options: defaultPalleteNames(),
+  settingCategory: SettingCategory.other,
+  proFeature: false,
+);
+
+SettingsModel resetDefaults = SettingsModel(
+  name: 'resetDefaults',
+  settingType: SettingType.button,
+  label: 'Reset Defaults',
+  tooltip: 'Reset all settings to defaults',
+  defaultValue: false,
+  icon: Icon(Icons.low_priority),
+  settingCategory: SettingCategory.tool,
+  onChange: (){resetAllDefaults();},
+  proFeature: false,
+  silent: true,
+);
+
+List<SettingsModel> initializeLifeAttributes() {
+
+  return [
+    reDraw,
+    zoomOpArt,
+
+    randomColors,
+    numberOfColors,
+    paletteType,
+    paletteList,
+    opacity,
+    resetDefaults,
+  ];
+
+
+}
+
+
+void paintLife(Canvas canvas, Size size, int seed, double animationVariable, OpArt opArt) {
+
+  rnd = Random(DateTime.now().millisecond);
+
+  if (paletteList.value != opArt.palette.paletteName){
+    opArt.selectPalette(paletteList.value);
+  }
+
+
+  // Initialise the canvas
+  double canvasWidth = size.width;
+  double canvasHeight = size.height;
+  double borderX = 0;
+  double borderY = 0;
+
+  // Work out the X and Y
+  int cellsX = (canvasWidth / (zoomOpArt.value)+1.9999999).toInt();
+  borderX = (canvasWidth - zoomOpArt.value * cellsX) / 2;
+
+  int cellsY = (canvasHeight / (zoomOpArt.value)+1.9999999).toInt();
+  borderY = (canvasHeight - zoomOpArt.value * cellsY) / 2;
+  borderY = (canvasHeight - zoomOpArt.value * cellsY) / 2;
+
+  int colourOrder = 0;
+  Color nextColor;
+
+  // Now make some art
+
+  // if first time through, initialise the squares
+  // print(squaresI.length);
+  if (squaresI == null)
+  {
+
+    // initialise the game
+    squaresI = List();
+
+    // Now make some art
+    for (int i = 0; i < cellsX; ++i) {
+
+      List squaresJ = List();
+
+      for (int j = 0; j < cellsY; ++j) {
+
+        // Choose the next colour
+        // colourOrder++;
+        // nextColor = opArt.palette.colorList[colourOrder % numberOfColors.value];
+        // if (randomColors.value) {
+        //   nextColor = opArt.palette.colorList[rnd.nextInt(numberOfColors.value)];
+        // }
+        // nextColor = nextColor.withOpacity(opacity.value);
+
+        // if (rnd.nextDouble()>0.5) nextColor = Colors.black;
+
+        nextColor = Color.fromRGBO((rnd.nextBool()) ? rnd.nextInt((256)) : 0, (rnd.nextBool()) ? rnd.nextInt((256)) : 0, (rnd.nextBool()) ? rnd.nextInt((256)) : 0, 1);
+
+        //save the colour
+        squaresJ.add(nextColor);
+
+        var x = borderX + i * zoomOpArt.value;
+        var y = borderY + j * zoomOpArt.value;
+
+        // draw the square
+        canvas.drawRect(
+            Offset(x,y) & Size(zoomOpArt.value, zoomOpArt.value),
+            Paint()
+              ..strokeWidth = 0.0
+              ..color = nextColor
+              ..isAntiAlias = false
+              ..style = PaintingStyle.fill);
+
+
+      }
+      squaresI.add(squaresJ);
+
+    }
+
+  }
+  else
+  {
+    List oldSquaresI = squaresI;
+    squaresI = List();
+
+    //play the game
+    for (int i = 0; i < cellsX; ++i) {
+
+      List squaresJ = List();
+
+      for (int j = 0; j < cellsY; ++j) {
+
+
+        List neighbours = List();
+
+        // if (i>0 && j>0) neighbours.add(oldSquaresI[i-1][j-1]);
+        // if (i>0 && j<cellsY-1) neighbours.add(oldSquaresI[i-1][j+1]);
+        // if (i<cellsX-1 && j>0) neighbours.add(oldSquaresI[i+1][j-1]);
+        // if (i<cellsX-1 && j<cellsY-1) neighbours.add(oldSquaresI[i+1][j+1]);
+
+        if (i>0) neighbours.add(oldSquaresI[i-1][j]);
+        if (j>0) neighbours.add(oldSquaresI[i][j-1]);
+        if (i<cellsX-1) neighbours.add(oldSquaresI[i+1][j]);
+        if (j<cellsY-1) neighbours.add(oldSquaresI[i][j+1]);
+
+        int neighboursRed = 0;
+        int neighboursGreen = 0;
+        int neighboursBlue = 0;
+        int neighboursAlive = 0;
+
+        neighbours.forEach((element) {
+          if (element.red > 0) neighboursRed++;
+          if (element.green > 0) neighboursGreen++;
+          if (element.blue > 0) neighboursBlue++;
+          if (HSLColor.fromColor(element).lightness>0) neighboursAlive++;
+        });
+
+        // Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+        // Any live cell with two or three live neighbours lives on to the next generation.
+        // Any live cell with more than three live neighbours dies, as if by overpopulation.
+        // Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+
+        bool wasAliveRed = (oldSquaresI[i][j].red>100);
+        bool nowAliveRed = ((wasAliveRed && (neighboursRed == 2 || neighboursRed==3)) || (!wasAliveRed && neighboursRed==3));
+        bool wasAliveGreen = (oldSquaresI[i][j].green>100);
+        bool nowAliveGreen = ((wasAliveGreen && (neighboursGreen == 2 || neighboursGreen==3)) || (!wasAliveGreen && neighboursGreen==3));
+        bool wasAliveBlue = (oldSquaresI[i][j].blue>100);
+        bool nowAliveBlue = ((wasAliveBlue && (neighboursBlue == 2 || neighboursBlue==3)) || (!wasAliveBlue && neighboursBlue==3));
+
+        Color nextColor = Color.fromRGBO((nowAliveRed) ? rnd.nextInt(156)+100 : 0, (nowAliveGreen) ? rnd.nextInt(156)+100 : 0, (nowAliveBlue) ? rnd.nextInt(156)+100 : 0, 1);
+
+        //save the colour
+        squaresJ.add(nextColor);
+
+        var x = borderX + i * zoomOpArt.value;
+        var y = borderY + j * zoomOpArt.value;
+
+        // draw the square
+        canvas.drawRect(
+            Offset(x,y) & Size(zoomOpArt.value, zoomOpArt.value),
+            Paint()
+              ..strokeWidth = 0.0
+              ..color = nextColor
+              ..isAntiAlias = false
+              ..style = PaintingStyle.fill);
+
+
+      }
+      squaresI.add(squaresJ);
+    }
+  }
+
+
+
+
+
+
+
+}
+
---
+++ lib/opart/opart_plasma.dart
@@ -0,0 +1,300 @@
+import 'package:flutter/material.dart';
+import '../main.dart';
+
+import '../model_opart.dart';
+import '../model_palette.dart';
+import '../model_settings.dart';
+import 'dart:math';
+import 'dart:core';
+
+List cells;
+List shades;
+int shadeOffset;
+int maxDepthOld;
+int numberOfColoursOld;
+
+SettingsModel reDraw = SettingsModel(
+  name: 'reDraw',
+  settingType: SettingType.button,
+  label: 'Redraw',
+  tooltip: 'Re-draw the picture with a different random seed',
+  defaultValue: false,
+  randomTrue: 1.0,
+  icon: Icon(Icons.refresh),
+  settingCategory: SettingCategory.tool,
+  proFeature: false,
+  onChange: (){seed = DateTime.now().millisecond;},
+  silent: true,
+);
+
+SettingsModel maximumDepth = SettingsModel(
+  settingType: SettingType.int,
+  name: 'maximumDepth',
+  label: 'Maximum Depth',
+  tooltip: 'The maximum recursion depth',
+  min: 2,
+  max: 8,
+  zoom: 100,
+  defaultValue: 8,
+  icon: Icon(Icons.line_weight),
+  settingCategory: SettingCategory.tool,
+  proFeature: false,
+);
+
+
+
+SettingsModel ratio = SettingsModel(
+  name: 'ratio',
+  settingType: SettingType.double,
+  label: 'Randomizer',
+  tooltip: 'The amount of randomness in the plasma',
+  min: 0.0,
+  max: 0.1,
+  randomMin: 0.05,
+  randomMax: 0.35,
+  zoom: 100,
+  defaultValue: 0.1,
+  icon: Icon(Icons.remove_red_eye),
+  settingCategory: SettingCategory.tool,
+  proFeature: false,
+);
+
+SettingsModel randomiseRatio = SettingsModel(
+  name: 'randomiseRatio',
+  settingType: SettingType.bool,
+  label: 'Randomise Ratio',
+  tooltip: 'Randomise the split ratio',
+  defaultValue: false,
+  icon: Icon(Icons.track_changes),
+  settingCategory: SettingCategory.tool,
+  proFeature: false,
+  silent: true,
+);
+
+SettingsModel lineWidth = SettingsModel(
+  settingType: SettingType.double,
+  name: 'lineWidth',
+  label: 'Outline Width',
+  tooltip: 'The width of the petal outline',
+  min: 0.0,
+  max: 10.0,
+  zoom: 100,
+  defaultValue: 3.0,
+  icon: Icon(Icons.line_weight),
+  settingCategory: SettingCategory.tool,
+  proFeature: false,
+);
+
+
+SettingsModel randomColors = SettingsModel(
+  name: 'randomColors',
+  settingType: SettingType.bool,
+  label: 'Random Colors',
+  tooltip: 'randomize the colours',
+  defaultValue: true,
+  icon: Icon(Icons.gamepad),
+  settingCategory: SettingCategory.tool,
+  proFeature: false,
+  silent: true,
+
+);
+
+
+SettingsModel paletteType = SettingsModel(
+  name: 'paletteType',
+  settingType: SettingType.list,
+  label: "Palette Type",
+  tooltip: "The nature of the palette",
+  defaultValue: "random",
+  icon: Icon(Icons.colorize),
+  options: [
+    'random',
+    'blended random',
+    'linear random',
+    'linear complementary'
+  ],
+  settingCategory: SettingCategory.palette,
+  onChange: (){generatePalette();},
+  proFeature: false,
+);
+
+
+
+SettingsModel resetDefaults = SettingsModel(
+  name: 'resetDefaults',
+  settingType: SettingType.button,
+  label: 'Reset Defaults',
+  tooltip: 'Reset all settings to defaults',
+  defaultValue: false,
+  icon: Icon(Icons.low_priority),
+  settingCategory: SettingCategory.palette,
+  onChange: (){},
+  silent: true,
+  proFeature: false,
+);
+
+List<SettingsModel> initializePlasmaAttributes() {
+
+  return [
+    reDraw,
+    maximumDepth,
+    ratio,
+    randomiseRatio,
+
+    lineColor,
+    lineWidth,
+    randomColors,
+    numberOfColors,
+    paletteType,
+    paletteList,
+    opacity,
+    resetDefaults,
+  ];
+
+
+}
+
+
+void paintPlasma(Canvas canvas, Size size, int seed, double animationVariable, OpArt opArt) {
+  rnd = Random(seed);
+
+  if (paletteList.value != opArt.palette.paletteName){
+    opArt.selectPalette(paletteList.value);
+  }
+
+  int smooth = 32;
+
+  if (reDraw.value == true || shades == null || shades.length != (opArt.palette.colorList.length-1)*smooth+1){
+
+    // generate the palette
+    shadeOffset = 0;
+    shades = null;
+
+    int numberOfColors = opArt.palette.colorList.length;
+    int numberOfShades = (numberOfColors-1)*smooth+1;
+    shades = List(numberOfShades);
+    shades[0] = opArt.palette.colorList[0];
+    for (int i=1; i<opArt.palette.colorList.length; i++) {
+      for (int j = 1; j <= smooth; j++) {
+        shades[(i-1)*smooth+j] = Color.fromRGBO(
+            (opArt.palette.colorList[i-1].red * (smooth - j) + opArt.palette.colorList[i].red * j)~/smooth,
+            (opArt.palette.colorList[i-1].blue * (smooth - j) + opArt.palette.colorList[i].blue * j)~/smooth,
+            (opArt.palette.colorList[i-1].green * (smooth - j) + opArt.palette.colorList[i].green * j)~/smooth,
+            1);
+      }
+    }
+
+  }
+
+  if (reDraw.value == true || maxDepthOld != maximumDepth.value) {
+    maxDepthOld = maximumDepth.value;
+
+    // generate the plasma
+    cells = null;
+    double randomizer = ratio.value;
+
+    // create the grid
+    int numberOfCells = pow(2, maximumDepth.value);
+    // print('numberOfCells: $numberOfCells');
+
+    cells = List(numberOfCells + 1);
+    for (int i = 0; i <= numberOfCells; i++) {
+      cells[i] = List(numberOfCells + 1);
+    }
+
+    // populate the corners
+    cells[0][0] = rnd.nextDouble();
+    cells[0][numberOfCells] = rnd.nextDouble();
+    cells[numberOfCells][0] = rnd.nextDouble();
+    cells[numberOfCells][numberOfCells] = rnd.nextDouble();
+
+    for (int d = numberOfCells; d > 1; d = d ~/ 2) {
+      for (int i = d ~/ 2; i <= numberOfCells; i = i + d) {
+        for (int j = d ~/ 2; j <= numberOfCells; j = j + d) {
+          square(i, j, d ~/ 2, randomizer, numberOfCells);
+        }
+      }
+      for (int i = d ~/ 2; i <= numberOfCells; i = i + d) {
+        for (int j = d ~/ 2; j <= numberOfCells; j = j + d) {
+          diamond(i, j - d ~/ 2, d ~/ 2, randomizer, numberOfCells);
+          diamond(i, j + d ~/ 2, d ~/ 2, randomizer, numberOfCells);
+          diamond(i - d ~/ 2, j, d ~/ 2, randomizer, numberOfCells);
+          diamond(i + d ~/ 2, j, d ~/ 2, randomizer, numberOfCells);
+        }
+      }
+    }
+  }
+
+
+
+
+
+
+
+  // Initialise the canvas
+  int numberOfCells = pow(2,maximumDepth.value);
+  double cellWidth = size.width / (numberOfCells + 1);
+  double cellHeight = size.height / (numberOfCells + 1);
+
+  // Now make some art
+  for (int i = 0; i <= numberOfCells; ++i) {
+    for (int j = 0; j <= numberOfCells; ++j) {
+
+      var p1 = [i*cellWidth, j*cellHeight];
+
+// print('i: $i j: $j cells[i][j]: ${cells[i][j]} shades.length: ${shades.length} shades[ (shadeOffset + (cells[i][j]*shades.length).toInt()) % shades.length]: ${shades[ (shadeOffset + (cells[i][j]*shades.length).toInt()) % shades.length]}');
+
+      Color nextColor = shades[ (shadeOffset + (cells[i][j]*shades.length).toInt()) % shades.length];
+      nextColor = (nextColor == null) ? Colors.white : nextColor;
+
+      // draw the square
+      canvas.drawRect(
+          Offset(p1[0], p1[1]) & Size(cellWidth, cellHeight),
+          Paint()
+            ..strokeWidth = 0.0
+            ..color = nextColor
+            ..isAntiAlias = false
+            ..style = PaintingStyle.fill);
+
+    }
+
+  }
+
+  // print('shadeOffset: $shadeOffset');
+  shadeOffset++;
+
+}
+
+
+// fill the centre of the square
+void square(int x, int y, int width, double randomizer, int numberOfCells){
+  // print('square x: $x y: $y width: $width randomizer: $randomizer numberOfCells: $numberOfCells');
+
+  double fill =
+    (
+        cells[x-width][y-width] +
+        cells[x-width][y+width] +
+        cells[x+width][y-width] +
+        cells[x+width][y+width]
+    )/4 +
+    rnd.nextDouble()*randomizer - randomizer/2;
+
+  cells[x][y] = (fill>1) ? 1 : (fill<0) ? 0 : fill;
+
+}
+// fill the centre of the diamond
+void diamond(int x, int y, int width, double randomizer, int numberOfCells){
+
+ double fill =
+      (
+          cells[(x+width <= numberOfCells) ? x+width : x+width-numberOfCells][y] +
+          cells[(x-width >= 0) ? x-width : x - width + numberOfCells][y] +
+          cells[x][(y+width <= numberOfCells) ? y+width : y+width-numberOfCells] +
+          cells[x][(y-width >= 0) ? y-width : y-width+numberOfCells]
+      )/4 +
+      rnd.nextDouble()*randomizer - randomizer/2;
+
+      cells[x][y] = (fill>1) ? 1 : (fill<0) ? 0 : fill;
+
+
+}
